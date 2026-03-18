fa355fcf2bb8d061fefd737bc30016e402d374a8
BC-Tech <prabhsharn>
2026-03-18 09:33:07 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+name = "judgeveer"
+age = 15
+booleans = True
+num = 3.14
+
+print(name)
---
+++ variable.py
@@ -0,0 +1,5 @@
+name = "prabhsharn"
+age = "14"
+school = "gobind sarvar"
+city_of_birth = "surrey"
+print(name,age,school,city_of_birth)
